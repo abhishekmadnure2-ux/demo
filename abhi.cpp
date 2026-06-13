1e138613b92f4e6c7bdc3e1ad4648f7157bcaa55
abhishekmadnure2-ux <abhishekmadnure2@gmail.com>
2026-06-14 00:50:16 +0530

    modified abhi.cpp

--- abhi.cpp
+++ abhi.cpp
@@ -4,6 +4,6 @@ int main()
     int a,b,c;
     printf("Enter the value of a, b and c:");
     scanf("%d%d%d",&a,&b,&c);
-    printf("The sum of a, b and c is %d",a+b+cgit );
+    printf("The sum of a, b and c is %d",a+b+c);
     return 0;
 }
